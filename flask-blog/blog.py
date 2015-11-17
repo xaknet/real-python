bc2ec559605d8f4c71bd3bac9af2f3204a4e7b4a
eldar219@gmail.com <eldar219@gmail.com>
2015-11-17 20:57:27 +0400

    Added files for blog flask app

---
+++ flask-blog/blog.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request, session, \
+    flash, redirect, url_for, g
+
+DATABASE = 'blog.db'
+
+app = Flask(__name__)
+
+app.config.from_object(__name__)
+
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
+
+
+
+@app.route('/')
+def login():
+    return render_template('login.html')
+
+@app.route('/main')
+def main():
+    return render_template('main.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
