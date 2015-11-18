6e0c9de56f3d58ad1daba4f17276ba5b1d88c95d
eldar219@gmail.com <eldar219@gmail.com>
2015-11-18 21:09:22 +0400

    End of flask-blog app

--- flask-blog/blog.py
+++ flask-blog/blog.py
@@ -1,7 +1,14 @@
 from flask import Flask, render_template, request, session, \
     flash, redirect, url_for, g
+from functools import wraps
+import sqlite3
 
 DATABASE = 'blog.db'
+USERNAME = 'admin'
+PASSWORD = 'admin'
+SECRET_KEY = '\xfc\xe4v+L\xbd\xd4xzA\xbc\xdc\x13]c\x1b\xdfb\xd1eN\x9e\xe2\x85'
+
+
 
 app = Flask(__name__)
 
@@ -10,16 +17,62 @@ app.config.from_object(__name__)
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
+def login_required(test):
+    @wraps(test)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return test(*args, **kwargs)
+        else:
+            flash('You need to login first.')
+            return redirect(url_for('login'))
+    return wrap
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME'] or \
+                request.form['password'] != app.config['PASSWORD']:
+                    error = 'Invalid Credentials. Please try again.'
+        else:
+            session['logged_in'] = True
+            return redirect(url_for('main'))
+
+    return render_template('login.html', error=error)
 
 @app.route('/main')
+@login_required
 def main():
-    return render_template('main.html')
+    g.db = connect_db()
+    cur = g.db.execute('select * from posts')
+    posts = [dict(title=row[0], post=row[1]) for row in
+             cur.fetchall()]
+    g.db.close()
+    return render_template('main.html', posts=posts)
 
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('login'))
+
+@app.route('/add', methods=['POST'])
+@login_required
+def add():
+    title = request.form['title']
+    post = request.form['post']
+    if not title or not post:
+        flash("All fields are required. Please try again.")
+        return redirect(url_for('main'))
+    else:
+        g.db = connect_db()
+        g.db.execute("""insert into posts (title, post) values
+            (?, ?)""", [request.form['title'], request.form['post']])
+        g.db.commit()
+        g.db.close()
+        flash('New entry was successfully posted')
+        return redirect(url_for('main'))
 
 if __name__ == '__main__':
     app.run(debug=True)
